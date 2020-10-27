ef59bc84a7c0b79088acc93bbc41c9a40628b442
= <matthewcb11@gmail.com>
2020-10-27 15:53:43 -0600

    Fixed portfolio links

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -63,30 +63,30 @@ function Portfolio() {
                     <div className="card">
                         <h5 className="card-title">Crowdpleaser</h5>
                         <p className="card-text">A music streaming web app in which users can host parties and invite friends with a unique party code. Everybody in the party can add songs to a shared queue and vote to determine which song plays next. Built using a Node.js, Express, MongoDB, and React.</p>
-                        <span> <a className="card-links" href="https://github.com/kylebremont/CrowdPleaser-server" target="_blank" rel="noopener noreferrer"><AiFillGithub size={42} /></a></span>
-                        <span><a className="card-links" href="https://crowdpleaser.love" target="_blank" rel="noopener noreferrer"><RiExternalLinkLine size={42} /></a></span>
+                        <span> <a className="card-link" href="https://github.com/kylebremont/CrowdPleaser-server" target="_blank" rel="noopener noreferrer"><AiFillGithub size={42} /></a></span>
+                        {/* <span><a className="card-links" href="https://crowdpleaser.love" target="_blank" rel="noopener noreferrer"><RiExternalLinkLine size={42} /></a></span> */}
                     </div>
                     <div className="card">
                         <h5 className="card-title">Sorting Algorithm Visualizer</h5>
                         <p className="card-text"> A react web app that visualizes different sorting algorithms on a histogram. Users can set size, speed, algorithm, and display style to visualize Merge Sort, Insertion Sort, Selection Sort, and Bubble Sort.</p>
-                        <a className="yup" href="https://github.com/mabthew/sorting-visualizer" target="_blank" rel="noopener noreferrer"><AiFillGithub size={42} /></a>
-                        <a className="yup" href="https://mabthew.github.io/sorting-visualizer/" target="_blank" rel="noopener noreferrer"><RiExternalLinkLine size={42} /></a>
+                        <a className="card-link" href="https://github.com/mabthew/sorting-visualizer" target="_blank" rel="noopener noreferrer"><AiFillGithub size={42} /></a>
+                        <a className="card-link" href="https://mabthew.github.io/sorting-visualizer/" target="_blank" rel="noopener noreferrer"><RiExternalLinkLine size={42} /></a>
                     </div>
 
                     <div className="card">
                         <h5 className="card-title">DropBucket</h5>
                         <p className="card-text">File-hosting service using a RESTful Django API to communicate with GCP Storage Buckets, synchronizing across multiple devices to manage downloading, uploading, and deleting files reflecting changes in the QT interface.</p>
-                        <a className="yup" href="https://github.com/mabthew/Dropbucket-deliverables" target="_blank" rel="noopener noreferrer"><AiFillGithub size={42} /></a>
+                        <a className="card-link" href="https://github.com/mabthew/Dropbucket-deliverables" target="_blank" rel="noopener noreferrer"><AiFillGithub size={42} /></a>
                     </div>
                     <div className="card">
                         <h5 className="card-title">Scrabbler</h5>
                         <p className="card-text">An anagram finder build using Node.js, Express, React, and Redis.</p>
-                        <a className="yup" href="https://github.com/mabthew/anagram" target="_blank" rel="noopener noreferrer"><AiFillGithub size={42} /></a>
+                        <a className="card-link" href="https://github.com/mabthew/anagram" target="_blank" rel="noopener noreferrer"><AiFillGithub size={42} /></a>
                     </div>
                     <div className="card">
                         <h5 className="card-title">This website!</h5>
                         <p className="card-text">I made this website from scratch using React, HTML, and CSS to practice web design.</p>
-                        <a className="yup" href="https://github.com/mabthew/personal-website" target="_blank" rel="noopener noreferrer"><AiFillGithub size={42} /></a>
+                        <a className="card-link" href="https://github.com/mabthew/personal-website" target="_blank" rel="noopener noreferrer"><AiFillGithub size={42} /></a>
                     </div>
                 </Carousel>
             </div>
